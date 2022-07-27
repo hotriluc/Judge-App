a59dc942baed9dac32b00658d3753dcf131e118b
Luc Ho <hotriluc@lucs-mbp.localdomain>
2022-07-27 14:42:50 +0200

    update dockerfile

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+bundle check || bundle install
+
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f $APP_PATH/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
