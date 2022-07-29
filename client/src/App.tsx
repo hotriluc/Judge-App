ac448210db5fc4429a3b897757f4fec11188b311
Luc Ho <hotriluc97@gmail.com>
2022-07-29 19:15:56 +0200

    setup router, redux and tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -16,21 +15,8 @@ const App = () => {
   // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello
-        </a>
-      </header>
+    <div>
+      <header>hello</header>
     </div>
   );
 };
