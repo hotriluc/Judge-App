b6e5963ef6407d51b18de367a7bb41585d84bd97
Luc Ho <hotriluc97@gmail.com>
2022-08-04 11:05:13 +0200

    issue jwt token

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+
+  # JWT Key stored as credentials
+  def jwt_key
+    Rails.application.credentials.jwt_key
+  end
+
+  # Get token from the request
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  # JWT Token generator
+  def issue_token(user)
+    JWT.encode({ user_id: user.id }, jwt_key)
+  end
+
 end
--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,27 +1,2 @@
 class UsersController < ApplicationController
-    def index
-        users = User.order("created_at DESC")
-        render json: users
-    end
-
-    def create
-        user = User.create(user_params)
-        render json: user
-    end
-
-    def show
-        user = User.find(params[:id])
-        render json: user
-    end
-
-    def update
-        user = User.find(params[:id])
-        user.update(user_param)
-        render json: user
-    end
-
-    private
-        def user_params
-            params.require(:user).permit(:first_name, :last_name, :password, :email)
-        end
 end
