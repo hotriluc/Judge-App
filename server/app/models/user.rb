7599edd3f56cbb067e483cb87b5cff91534adb92
Luc Ho <hotriluc97@gmail.com>
2022-08-07 12:15:19 +0200

    setup robocop

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name, presence: {message: 'First name field is empty'}
-  validates :last_name, presence: {message: 'Last name field is empty'}
-  validates :email, uniqueness: {message: 'This email is already registered'}, email: true
+  validates :first_name, presence: { message: 'First name field is empty' }
+  validates :last_name, presence: { message: 'Last name field is empty' }
+  validates :email, uniqueness: { message: 'This email is already registered' }, email: true
 end
