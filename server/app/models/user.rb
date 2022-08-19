a30bf2947c64ddb0dd6a811d3eac27024f3252e7
Luc Ho <hotriluc97@gmail.com>
2022-08-19 18:21:37 +0200

    create course model

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  # self.ignored_columns = %w[password_digest]
+  has_many :courses, dependent: :destroy, foreign_key: 'owner_id'
 
   validates :first_name, presence: { message: 'First name field is empty' }
   validates :last_name, presence: { message: 'Last name field is empty' }
   validates :email, uniqueness: { message: 'This email is already registered' }, email: true
-
 end
