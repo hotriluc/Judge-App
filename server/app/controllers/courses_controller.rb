8b0e56410da88ae1053ecca52d9420389bc396b7
Luc Ho <hotriluc97@gmail.com>
2022-08-20 15:27:01 +0200

    course crud

---
+++ server/app/controllers/courses_controller.rb
@@ -0,0 +1,37 @@
+class CoursesController < ApplicationController
+  before_action :set_course, only: %i[show update]
+
+  # GET /courses
+  def index
+    @courses = current_user.courses
+    render json: @courses
+  end
+
+  # GET /courses/:id
+  def show
+    render json: @course
+  end
+
+  # PATCH /course/:id
+  def update
+    @course.update(course_params)
+    render json: @course
+  end
+
+  # DELETE /course/:id
+  def destroy
+    Course.destroy(@course.id)
+    render json: { message: 'success' }
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description)
+  end
+
+  def set_course
+    @course = Course.find_by(id: params['id'])
+    render json: { error: 'No such course exists' }, status: 400 if @course.nil?
+  end
+end
