28cf778f81d3b70024043ea2ad85c8c43274cd8d
Luc Ho <hotriluc97@gmail.com>
2022-08-21 21:01:35 +0200

    check useEffect courses

--- client/src/components/Dashboard/CoursesTable.tsx
+++ client/src/components/Dashboard/CoursesTable.tsx
@@ -1,5 +1,7 @@
 import { Table } from '@mantine/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAppDispatch } from '../../hooks/app-hooks';
+import { getCourses } from '../../services/CoursesService';
 // import { Link } from 'react-router-dom';
 
 const courseList = [
@@ -8,6 +10,24 @@ const courseList = [
 ];
 
 const CoursesTable = () => {
+  const dispatch = useAppDispatch();
+  const token = localStorage.getItem('token');
+  const [newPage, setNewPage] = useState(0);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getCourses(token));
+    }
+  }, [newPage]);
+
+  const onClickNextPageHanlder = () => {
+    setNewPage((newPage: number) => newPage + 1);
+  };
+
+  const onClickPrevPageHandler = () => {
+    setNewPage((newPage: number) => newPage - 1);
+  };
+
   const rows = courseList.map((element) => (
     <tr key={element.name}>
       <td>{element.id}</td>
@@ -16,17 +36,22 @@ const CoursesTable = () => {
   ));
 
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>Title</th>
-          <th>User count</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </Table>
+    <div>
+      <button onClick={onClickPrevPageHandler}>Prev</button>
+      <button onClick={onClickNextPageHanlder}>Next</button>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Title</th>
+            <th>User count</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
+    </div>
   );
 };
 
