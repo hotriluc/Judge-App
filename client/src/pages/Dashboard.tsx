7331d0f73f15e9f91a712c296c007e0400b77c07
Luc Ho <hotriluc97@gmail.com>
2022-08-21 20:35:51 +0200

    wrapper component for second part of the app

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Wrapper from '../components/Layout/Wrapper';
 
 const Dashboard = () => {
   return (
-    <div>
+    <Wrapper>
       <h1>dashboard</h1>
       <Outlet />
-    </div>
+    </Wrapper>
   );
 };
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Wrapper from '../components/Layout/Wrapper';
 
 const Home = () => {
-  return <div>last courses</div>;
+  return (
+    <Wrapper>
+      <h1>this is home page</h1>
+    </Wrapper>
+  );
 };
 
 export default Home;
