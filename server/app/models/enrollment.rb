176c3182ddd921ccaa9aa3a59f8011b032048cc1
Luc Ho <hotriluc97@gmail.com>
2022-09-24 18:27:31 +0200

    unique student for course

--- server/app/models/enrollment.rb
+++ server/app/models/enrollment.rb
@@ -1,4 +1,7 @@
 class Enrollment < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  # Restrict adding same user to the same course
+  validates_uniqueness_of :course_id, scope: :user_id
 end
